d585bbf52e8d423ee7db7b279dfae1ed5e25a624
veselinovic93 <veselinovic93@hotmail.com>
2024-12-01 01:51:43 +0100

    feature: add pointer events none to active link

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 window.addEventListener("load", () => {
+  const navItem = document.querySelectorAll(".nav-item");
   const copyright = document.querySelector(".copyright");
   if (!copyright) return;
 
   copyright.textContent = `Copyright © ${new Date().getFullYear()} Obrada Vektorskih Slika`;
+
+  navItem.forEach((item) => {
+    item.classList.contains("active")
+      ? (item.style.pointerEvents = "none")
+      : "";
+  });
 });
